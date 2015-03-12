e356ee4711fd8ed34b3f6fa5ac9de6ffbe95dd41
MKS-Student <mks.student@hackreactor.com>
2015-03-11 20:23:34 -0700

    wip

--- src/solvers.js
+++ src/solvers.js
@@ -14,8 +14,38 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = []; 
+  var board = new Board({'n':n});
+  var counter =0;
+  var nestedRow = [];
+  //get every possible value
+  var checkRooks = function(board){
+    //check all conflicts, if there is a conflicts
+    //toggle pices on board
 
+    for (var row = 0; row <n; row++){
+      var currentRow = this.get(row);
+      for(var col = 0; col<n; col++){
+        if(!(currentRow[col]) && counter <= n){
+          board.togglePiece(row,col);
+          if(!board.hasAnyRooksConflicts()){ 
+            nestedRow.push(1);
+            counter++;
+            checkRooks(board);
+          } else {
+            nestedRow.push(0);
+            board.togglePiece(row,col); 
+          }
+        }
+      }
+      solution.push(nestedRow);
+    }
+  }
+  checkRooks(board);
+  
+  //if there are no conflicts, increment solution by 1
+
+  
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
@@ -27,6 +57,7 @@ window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  debugger;
   return solutionCount;
 };
 
